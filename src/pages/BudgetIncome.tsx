d397b32594f8b185fd3a4d3feb51086df93a3104
marlepo <marcinlpotrykus@gmail.com>
2022-12-13 17:04:24 +0100

    Update 0.0.5 (#5)
    * adding new feature: TableRecentlyAdded
    
    * TableRecentlyAdded into income & spendings page

--- src/pages/BudgetIncome.tsx
+++ src/pages/BudgetIncome.tsx
@@ -1,12 +1,16 @@
 import { t } from "i18next";
 import { BackToHome } from "../components/BackToHome";
+import { TableRecentlyAdded } from "../components/TableRecentlyAdded";
+import { TransactionItemType } from "../store/Item";
 
 export const BudgetIncome = () => {
   return (
     <>
       <BackToHome />
 
-      <h1 className="text-white">{t("Incomes")}</h1>
+      <h1 className="text-white my-3">{t("Incomes")}</h1>
+
+      <TableRecentlyAdded type={TransactionItemType.INCOME} />
     </>
   );
 };
--- src/pages/BudgetSpendings.tsx
+++ src/pages/BudgetSpendings.tsx
@@ -1,12 +1,16 @@
 import { t } from "i18next";
 import { BackToHome } from "../components/BackToHome";
+import { TableRecentlyAdded } from "../components/TableRecentlyAdded";
+import { TransactionItemType } from "../store/Item";
 
 export const BudgetSpendings = () => {
   return (
     <>
       <BackToHome />
 
-      <h1 className="text-white">{t("Spendings")}</h1>
+      <h1 className="text-white my-3">{t("Spendings")}</h1>
+
+      <TableRecentlyAdded type={TransactionItemType.OUTCOME} />
     </>
   );
 };
