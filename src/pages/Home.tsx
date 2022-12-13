bd899a1d8f44c9e306db8ffa4abb7a5496b93be2
marlepo <marcinlpotrykus@gmail.com>
2022-12-13 11:45:59 +0100

    implementing new feature for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { Balance } from "./parts/Balance";
+import { BudgetActionGroup } from "./parts/BudgetActionGroup";
 import { SpendingsAndIncomes } from "./parts/SpendingsAndIncomes";
 
 export const Home = () => {
@@ -6,6 +7,7 @@ export const Home = () => {
     <main className="flex flex-col h-full gap-4 relative">
       <Balance />
       <SpendingsAndIncomes />
+      <BudgetActionGroup />
     </main>
   );
 };
