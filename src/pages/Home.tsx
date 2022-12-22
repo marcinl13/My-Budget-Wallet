bc64448e1bcaeb72c50d1145a9010efb057be1ae
marlepo <marcinlpotrykus@gmail.com>
2022-12-22 20:21:45 +0100

    Update 0.0.11 (#12)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
+import { t } from "i18next";
 import { Balance } from "./parts/Balance";
 import { BudgetActionGroup } from "./parts/BudgetActionGroup";
+import { ExpenseCategoryCarousel } from "./parts/ExpenseCategoryCarousel";
 import { SpendingAndIncomes } from "./parts/SpendingAndIncomes";
 
 export const Home = () => {
@@ -8,6 +10,9 @@ export const Home = () => {
       <Balance />
       <SpendingAndIncomes />
       <BudgetActionGroup />
+
+      <h3 className="text-white text-left font-bold">{t("Recent category expenses")}</h3>
+      <ExpenseCategoryCarousel />
     </main>
   );
 };
